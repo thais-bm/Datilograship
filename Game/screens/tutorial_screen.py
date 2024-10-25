df8c7c87e77028fd8321b4448b13ba7476717de8
Thais Carolina <thaisbraga969@gmail.com>
2024-10-24 20:24:06 -0400

    Update: Game_tutorial (see description)
    Changes
    -> Added Cyan color to hand
    -> Added Instructions texts to tutorial
    -> Enter button in debug mode: if u press it, it closes the game

--- Game/screens/tutorial_screen.py
+++ Game/screens/tutorial_screen.py
@@ -6,6 +6,12 @@ from entities.keyboard import Keyboard
 from entities.image import Image
 from entities.text import Text
 
+import pygame
+from entities.key_listener import KeyListener
+from resource.fonts import *
+from resource.color import *
+
+
 class Tutorial_Screen(Screen):
     def populate(self):
         self.keyboard = Keyboard(Game_Manager.screen_width * 0.2, Game_Manager.screen_height * 0.2)
@@ -13,3 +19,24 @@ class Tutorial_Screen(Screen):
                            center = (Game_Manager.screen_width * 0.7, Game_Manager.screen_height * 0.4),
                            width = Game_Manager.screen_width * 0.2,
                            height = Game_Manager.screen_height * 0.3)
+
+        # Return/Enter keyboard Listener -> "Press enter to pass the tutorial"
+        # Debug mode: It closes the game
+        self.return_listener = KeyListener([pygame.K_RETURN], [Game_Manager.close_game])
+
+        # Text setups
+        self.title = Text(content = "Como jogar",
+                              center=(Game_Manager.screen_width * 0.5, Game_Manager.screen_height * 0.1),
+                              size = 60,
+                              font = SEGA,
+                              color = WHITE)
+        self.say = Text(content="Teste o funcionamento do teu teclado aqui!",
+                          center=(Game_Manager.screen_width * 0.5, Game_Manager.screen_height * 0.2),
+                          size=40,
+                          font=SEGA,
+                          color=WHITE)
+        self.enter = Text(content = "Aperte ENTER para prosseguir",
+                              center=(Game_Manager.screen_width * 0.5, Game_Manager.screen_height * 0.8),
+                              size = 60,
+                              font = SEGA,
+                              color = WHITE)
