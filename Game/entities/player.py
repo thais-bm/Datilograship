5cb1a7e85f0ae0711f0dd27a55cfa4ac81521fc4
Joao Pedro Telles Paes <jptpa.snf24@uea.edu.br>
2024-10-27 00:22:49 -0400

    Added function to make player ship rotate to target specified positions

--- Game/entities/player.py
+++ Game/entities/player.py
@@ -1,11 +1,14 @@
 from entities.entity_base import Entity
 from entities.image import Image
+import math
+import pygame
 
 class Player(Entity):
     def __init__(self, center, sprite_path, hitbox = None, width = None, height = None, layer = 1):
         super().__init__(layer = layer)
         self.life = 3
         self.sprite = Image(path = sprite_path, center = center, width = width, height = height)
+        self.base_sprite = self.sprite.image
 
         self.center = center
 
@@ -13,3 +16,11 @@ class Player(Entity):
 
         if hitbox == None:
             hitbox = self.sprite.image.get_rect()
+    #function to rotate player shooty thing sprite to aim for specified coordinates
+    def rotate(self, target_pos):
+        dx = target_pos[0] - self.center[0]
+        dy = target_pos[1] - self.center[1]
+        tan = dx/dy
+        self.angle = math.degrees(math.atan(tan))
+
+        self.sprite.image = pygame.transform.rotate(self.base_sprite, self.angle)
